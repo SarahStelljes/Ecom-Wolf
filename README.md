a495e7b72e4ede736843fbdb47f21c776084c384
Battlehart <sarah.stelljes@gmail.com>
2022-09-04 10:49:59 -0400

    edited models and README.md. Models should now meet requirements.

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Ecom-Wolf
+
+## Built With
+* JavaScript
+### With the help of
+* NodeJS
+* Sequelize
+* Mysql2
+* Express
+* dotenv
--- models/Category.js
+++ models/Category.js
@@ -8,15 +8,13 @@ Category.init(
   {
     id: {
       type: DataTypes.INTEGER,
+      allowNull: false,
       primaryKey: true,
       autoIncrement: true
     },
     category_name:{
       type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: [4]
-      }
+      allowNull: false
     }
   },
   {
